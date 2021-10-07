eb866faeee22b80615d150cbc64f40b3b214493c
PetrovIgorA <petrov.igor.a@gmail.com>
2021-10-07 12:31:43 +0300

    03_Multifile final done

--- 03_Multifile/fun.c
+++ 03_Multifile/fun.c
@@ -2,13 +2,14 @@
 #include "outlib.h"
 void output(char* str)
 {
-    printf("%d: %s\012", Count++, str);
+printf("%d: %s\012", Count++, str);
 }
 
 void usage(char* prog)
 {
+    // Change %s to %.6s for correct logs comparison
     fprintf(stderr,
-            "%s v%.2f: Print all arguments\012\t"
-            "Usage: %s arg1 [arg2 […]]\012",
-            prog, VERSION, prog);
+        "%.6s v%.2f: Print all arguments\012\t"
+        "Usage: %.6s arg1 [arg2 […]]\012",
+        prog, VERSION, prog);
 }
